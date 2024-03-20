4a93bb34b759caf1044540c6efd11fb8f48ccf08
BOBOeternal <106109437+BOBOeternal@users.noreply.github.com>
2024-03-20 14:03:14 +0000

    mvc

--- src/main/java/com/example/recipes/controller/RecipeController.java
+++ src/main/java/com/example/recipes/controller/RecipeController.java
@@ -2,7 +2,9 @@ package com.example.recipes.controller;
 
 
 import com.example.recipes.model.Recipe;
+import org.springframework.ui.Model;
 import com.example.recipes.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,29 @@ public class RecipeController {
     private final RecipeRepository recipeRepository;
 
 
+    @Autowired
     public RecipeController(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
+//    @GetMapping
+//    public Iterable<Recipe> findAll() {
+//        return recipeRepository.findAll();
+//    }
+//
+//    @GetMapping
+//    public String showRecipes(Model model) {
+//        model.addAttribute("recipes", recipeRepository.findAll());
+//        return "recipes";
+//    }
+
     @GetMapping
-    public Iterable<Recipe> findAll() {
-        return recipeRepository.findAll();
+    public String showRecipes(Model model) {
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        model.addAttribute("recipes", recipes);
+        return "recipes"; // Thymeleaf template name
     }
+
+
+
 }
--- src/main/java/com/example/recipes/repository/RecipeRepository.java
+++ src/main/java/com/example/recipes/repository/RecipeRepository.java
@@ -4,4 +4,5 @@ import com.example.recipes.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Integer>{
+
 }
